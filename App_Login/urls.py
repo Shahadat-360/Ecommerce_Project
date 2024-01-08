288aaea114e96a31985ad3654e100dc1aa646fe1
MD.Shahadat <md_shahadat_@outlook.com>
2024-01-08 21:55:29 +0600

    setting up project

---
+++ App_Login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.shortcuts import redirect,reverse,render
+
+app_name = 'App_Login'
+
+urlpatterns = [
+
+]
---
+++ App_Shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.shortcuts import redirect,render,reverse
+
+app_name = 'App_Shop'
+
+
+urlpatterns = [
+
+]
