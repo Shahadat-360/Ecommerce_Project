604727fa2003b42ab2b329ab4834bf75c8ef7bcb
MD.Shahadat <md_shahadat_@outlook.com>
2024-01-10 22:42:23 +0600

    Product Page

--- App_Shop/urls.py
+++ App_Shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'App_Shop'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='index'),
+    path('product/<int:pk>/', views.ProductDetail.as_view(), name='product_detail')
 ]
--- App_Shop/views.py
+++ App_Shop/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 # view import
 from django.views.generic import ListView, DetailView
@@ -9,3 +10,6 @@ from App_Shop.models import Product, Category
 class Home(ListView):
     model = Product
     template_name = 'App_Shop/home.html'
+class ProductDetail(DetailView,LoginRequiredMixin):
+    model = Product
+    template_name = 'App_Shop/product_detail.html'
